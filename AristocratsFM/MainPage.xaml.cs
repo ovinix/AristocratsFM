a2aac948babe5c24ca96676d61575646f852a2df
Volodymyr <dev.shvvolodymyr@hotmail.com>
2015-10-23 19:13:32 +0300

    Version 1.0.0.4

---
+++ AristocratsFM/MainPage.xaml.cs
@@ -0,0 +1,442 @@
+﻿using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Playback;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Popups;
+using System.Diagnostics;
+using Windows.Storage;
+using Windows.Data.Xml.Dom;
+
+namespace AristocratsFM
+{
+    public sealed partial class MainPage : Page
+    {
+        private MediaPlayer _mediaPlayer;
+        private bool _isTimerStarted = false;
+        private string _csUrl = "http://air.aristocrats.fm/cs-amusic.php";
+        private string _toPlay = "http://144.76.79.38:8000/amusic-128";
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.InitializeMediaPlayer();
+
+            this.UpdateCurrentStation();            
+            
+            this.UpdateButtonImage();         
+            
+            this.StartTimer();       
+        }
+
+        private void InitializeMediaPlayer()
+        {
+            Debug.WriteLine("InitializeMediaPlayer() - Begin");
+            // Launching the BackgroundAudioTask
+            _mediaPlayer = BackgroundMediaPlayer.Current;
+            _mediaPlayer.CurrentStateChanged += _mediaPlayer_CurrentStateChanged;
+
+            Debug.WriteLine("InitializeMediaPlayer() - _mediaPlayer.CurrentState:" + _mediaPlayer.CurrentState.ToString());
+
+            BackgroundMediaPlayer.MessageReceivedFromBackground += BackgroundMediaPlayer_MessageReceivedFromBackground;
+
+            Debug.WriteLine("InitializeMediaPlayer() - End");
+        }
+
+        #region BackgroundMediaPlayer_Events
+        async void BackgroundMediaPlayer_MessageReceivedFromBackground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            ValueSet valueSet = e.Data;
+            foreach (string key in valueSet.Keys)
+            {
+                switch (key)
+                {
+                    case "Exited":
+                        Debug.WriteLine("BackgroundMediaPlayer_MessageReceivedFromBackground() - Exited message recieved");                     
+                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            _mediaPlayer = null;
+                            PlayButton.Background = this.Resources["PlayImageBrush"] as Brush;
+                            Buffering.Text = "";
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        async void _mediaPlayer_CurrentStateChanged(MediaPlayer sender, object args)
+        {
+            switch (sender.CurrentState)
+            {
+                case MediaPlayerState.Buffering:
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        Buffering.Text = "Buffering...";
+                    });
+                    break;
+                case MediaPlayerState.Closed:
+                    break;
+                case MediaPlayerState.Opening:
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        // Updating current theme in case user changed station and clicked UVC Play button
+                        // with previous station stream
+                        this.UpdateCurrentStation();
+                        Buffering.Text = "Opening...";
+                    });
+                    break;
+                case MediaPlayerState.Paused:
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        this.UpdateButtonImage();
+                    });
+                    break;
+                case MediaPlayerState.Playing:
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        this.UpdateButtonImage();
+                        Buffering.Text = "";
+                    });
+                    break;
+                case MediaPlayerState.Stopped:
+                    break;
+                default:
+                    break;
+            }
+        }
+        #endregion BackgroundMediaPlayer_Events
+
+        #region Setting_Page_Content
+        private async void GetCurrentSong(string url)
+        {
+            try
+            {
+                // WP cashes responses with same url
+                string newUrl = url + "?temp=" + DateTime.Now.Millisecond;
+                string page = await new HttpClient().GetStringAsync(newUrl);
+
+                string artist = "", song = "";
+
+                if (_csUrl.Equals("http://aristocrats.fm/service/NowOnAir.xml"))
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(page);
+
+                    var nodes = xmlDoc.GetElementsByTagName("Artist");
+                    artist = nodes[0].Attributes.GetNamedItem("name").InnerText;
+
+                    nodes = xmlDoc.GetElementsByTagName("Song");
+                    song = nodes[0].Attributes.GetNamedItem("title").InnerText;
+
+                    if (artist == "" && song == "")
+                    {
+                        song = "Прямой эфир";
+                        artist = "Радио Аристократы";
+                    }
+                }
+                else
+                {
+                    page = page.Replace("h4", "");
+                    page = page.Replace("h5", "");
+                    page = page.Replace("/", "");
+
+                    string[] separators = { "<>" };
+                    string[] CurrentSong = page.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                    artist = CurrentSong[0];
+                    if (CurrentSong.Length > 1)
+                    {
+                        song = CurrentSong[1];
+                    }
+                    else
+                    {
+                        song = "";
+                    }
+                }
+
+                Artist.Text = artist;
+                Song.Text = song;
+            }
+            catch (Exception)
+            {
+                Artist.Text = "";
+                Song.Text = "";
+            }
+        }
+
+        private async void UpdateCurrentStation()
+        {
+            Debug.WriteLine("GetCurrentStation() - Asking for station");
+            // Loading current station from app settings
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("CurrentStation"))
+            {
+                _toPlay = ApplicationData.Current.LocalSettings.Values["CurrentStation"].ToString();
+
+                switch (_toPlay)
+                {
+                    case "http://144.76.79.38:8000/amusic-128":
+                        _csUrl = "http://air.aristocrats.fm/cs-amusic.php";
+                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            this.GetCurrentSong(_csUrl);
+                            this.SetCurrentTheme();
+                        });
+                        break;
+                    case "http://144.76.79.38:8000/live2-64":
+                        _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            this.GetCurrentSong(_csUrl);
+                            RadioButton64.IsChecked = true;
+                            this.SetCurrentTheme();                            
+                        });
+                        break;
+                    case "http://144.76.79.38:8000/live2":;
+                        _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            this.GetCurrentSong(_csUrl);
+                            RadioButton128.IsChecked = true;
+                            this.SetCurrentTheme();                            
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void UpdateButtonImage()
+        {
+            Debug.WriteLine("SetButtonImage()");
+            if (_mediaPlayer.CurrentState == MediaPlayerState.Playing)
+            {
+                //PlayButton.Template = PauseButtonTemplate;
+
+                PlayButton.Background = this.Resources["PauseImageBrush"] as Brush;
+                Debug.WriteLine("   SetButtonImage() - PauseImage");
+            }
+            else if (_mediaPlayer.CurrentState == MediaPlayerState.Paused || _mediaPlayer.CurrentState == MediaPlayerState.Closed)
+            {
+                //PlayButton.Template = PlayButtonTemplate;
+
+                PlayButton.Background = this.Resources["PlayImageBrush"] as Brush;
+                Debug.WriteLine("   SetButtonImage() - PlayImage");
+            }
+        }
+
+        private void SetCurrentTheme()
+        {
+            Debug.WriteLine("SetCurrentTheme()");
+            if (!_csUrl.Equals("http://air.aristocrats.fm/cs-amusic.php"))
+            {
+                //Color cl = new Color();
+                //cl.R = 153;
+                //cl.G = 43;
+                //cl.B = 53;
+                //cl.A = 255;
+                //this.Background = new SolidColorBrush(cl);
+
+                this.Background = this.Resources["RedThemeBrush"] as Brush;
+
+                ButtonAristocrats.BorderBrush = new SolidColorBrush(Colors.White);
+                ButtonAMusic.BorderBrush = null;
+
+                RadioButton64.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                RadioButton128.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                Debug.WriteLine("   SetCurrentTheme() - RedThemeBrush");
+            }
+            else
+            {
+                //Color cl = new Color();
+                //cl.R = 0;
+                //cl.G = 48;
+                //cl.B = 74;
+                //cl.A = 255;
+                //this.Background = new SolidColorBrush(cl);
+
+                this.Background = this.Resources["BlueThemeBrush"] as Brush;
+
+                ButtonAMusic.BorderBrush = new SolidColorBrush(Colors.White);
+                ButtonAristocrats.BorderBrush = null;
+
+                RadioButton64.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                RadioButton128.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+                Debug.WriteLine("   SetCurrentTheme() - BlueThemeBrush");
+            }
+        }
+        #endregion Setting_Page_Content
+
+        #region Media_Managment
+        private void PauseMedia()
+        {
+            var message = new ValueSet{ { "Pause", "" } };
+            BackgroundMediaPlayer.SendMessageToBackground(message);
+        }
+
+        private void PlayMedia(string toPlay)
+        {
+            // Saving current station
+            if (!ApplicationData.Current.LocalSettings.Values.ContainsKey("CurrentStation"))
+            {
+                ApplicationData.Current.LocalSettings.Values.Add("CurrentStation", toPlay);
+            }
+            else
+            {
+                ApplicationData.Current.LocalSettings.Values["CurrentStation"] = toPlay;
+            }
+
+            var message = new ValueSet { { "Play", toPlay } };
+            BackgroundMediaPlayer.SendMessageToBackground(message);
+        }
+        #endregion Media_Managment
+
+        #region Timer
+        private void StartTimer()
+        {
+            Debug.WriteLine("StartTimer()");
+            if (_mediaPlayer.CurrentState == MediaPlayerState.Closed)
+            {
+                GetCurrentSong(_csUrl);
+            }
+            
+            _isTimerStarted = true;
+            //  DispatcherTimer setup
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler<object>(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 8);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            if (_isTimerStarted)
+            {
+                GetCurrentSong(_csUrl);
+            }
+        }
+        #endregion Timer
+
+        #region ButtonClick_Events
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Line below added to escape crashes then UVC was intercepted by another app
+            if (_mediaPlayer == null)
+            {
+                this.InitializeMediaPlayer();
+            }
+
+            switch (_mediaPlayer.CurrentState)
+            {
+                case MediaPlayerState.Buffering:
+                    break;
+                case MediaPlayerState.Closed:
+                    PlayMedia(_toPlay);
+                    break;
+                case MediaPlayerState.Opening:
+                    break;
+                case MediaPlayerState.Paused:
+                    PlayMedia(_toPlay);
+                    break;
+                case MediaPlayerState.Playing:
+                    PauseMedia();
+                    break;
+                case MediaPlayerState.Stopped:
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        private void ButtonAristocrats_Click(object sender, RoutedEventArgs e)
+        {
+            if (ButtonAristocrats.BorderBrush == null)
+            {
+                PauseMedia();
+
+                if (RadioButton64.IsChecked == true)
+                {
+                    _toPlay = "http://144.76.79.38:8000/live2-64";
+                    _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+                }
+                else if (RadioButton128.IsChecked == true)
+                {
+                    _toPlay = "http://144.76.79.38:8000/live2";
+                    _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+                }
+                else
+                {
+                    RadioButton64.IsChecked = true;
+                    _toPlay = "http://144.76.79.38:8000/live2-64";
+                    _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+                }
+                
+                SetCurrentTheme();
+
+                GetCurrentSong(_csUrl);
+            }
+        }
+
+        private void ButtonAMusic_Click(object sender, RoutedEventArgs e)
+        {
+            if (ButtonAMusic.BorderBrush == null)
+            {
+                PauseMedia();
+
+                _toPlay = "http://144.76.79.38:8000/amusic-128";
+                _csUrl = "http://air.aristocrats.fm/cs-amusic.php";
+
+                SetCurrentTheme();
+
+                GetCurrentSong(_csUrl);
+            }
+        }        
+        #endregion ButtonClick_Events
+
+        private void RadioButton64_Checked(object sender, RoutedEventArgs e)
+        {
+            _toPlay = "http://144.76.79.38:8000/live2-64";
+            _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+        }
+
+        private void RadioButton128_Checked(object sender, RoutedEventArgs e)
+        {
+            _toPlay = "http://144.76.79.38:8000/live2";
+            _csUrl = "http://aristocrats.fm/service/NowOnAir.xml";
+        }
+
+        private void RadioButton64_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (BackgroundMediaPlayer.Current.CurrentState == MediaPlayerState.Playing || BackgroundMediaPlayer.Current.CurrentState == MediaPlayerState.Buffering)
+            {
+                PauseMedia();
+            }
+        }
+
+        private void RadioButton128_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (BackgroundMediaPlayer.Current.CurrentState == MediaPlayerState.Playing || BackgroundMediaPlayer.Current.CurrentState == MediaPlayerState.Buffering)
+            {
+                PauseMedia();
+            }
+        }
+    }
+}
---
+++ BackgroundAudioTask/BackgroundAudioTask.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.UI.Popups;
+
+namespace BackgroundAudioTask
+{
+    public sealed class BackgroundAudioTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+        private SystemMediaTransportControls _systemMediaTransportControl;
+        private string _nowPlaying { get; set; }
+
+        // This is for "every 5 min cancelation on pause" bug
+        private bool _isBackgroundTaskRunning = false;
+        private AutoResetEvent _BackgroundStarted = new AutoResetEvent(false);
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Enable system transport controls for the current view. 
+            _systemMediaTransportControl = SystemMediaTransportControls.GetForCurrentView();
+            _systemMediaTransportControl.IsEnabled = true;
+            _systemMediaTransportControl.IsPauseEnabled = true;
+            _systemMediaTransportControl.IsPlayEnabled = true;
+            _systemMediaTransportControl.ButtonPressed += MediaTransportControlButtonPressed;
+            
+            // Handle events
+            BackgroundMediaPlayer.MessageReceivedFromForeground += BackgroundMediaPlayer_MessageReceivedFromForeground;
+            BackgroundMediaPlayer.Current.CurrentStateChanged += Current_CurrentStateChanged;
+            BackgroundMediaPlayer.Current.BufferingStarted += Current_BufferingStarted;
+            BackgroundMediaPlayer.Current.MediaFailed += Current_MediaFailed;
+
+            // Handle the closing of the background task correctly
+            taskInstance.Canceled += taskInstance_Canceled;
+            taskInstance.Task.Completed += Task_Completed;
+
+            _deferral = taskInstance.GetDeferral();
+
+            // TODO: load _nowPlaying from app settings
+            if (ApplicationData.Current.LocalSettings.Values.ContainsKey("CurrentStation"))
+            {
+                _nowPlaying = ApplicationData.Current.LocalSettings.Values["CurrentStation"].ToString();
+                //ApplicationData.Current.LocalSettings.Values.Remove("NowPlaying");
+            }
+
+            _BackgroundStarted.Set();
+            _isBackgroundTaskRunning = true;
+            // END
+
+            Debug.WriteLine("BackgroundAudioTask - Started");
+        }
+
+        void Current_MediaFailed(MediaPlayer sender, MediaPlayerFailedEventArgs args)
+        {
+            // TODO failed to open stream
+            // Sending message to null foreground mediaplayer
+            var message = new ValueSet { { "Exited", "" } };
+            BackgroundMediaPlayer.SendMessageToForeground(message);
+
+            BackgroundMediaPlayer.Shutdown();
+            _deferral.Complete();
+        }
+
+        async void Current_BufferingStarted(MediaPlayer sender, object args)
+        {
+            try
+            {
+                BackgroundMediaPlayer.Current.Pause();
+                while (BackgroundMediaPlayer.Current.BufferingProgress < 1)
+                {
+                    Debug.WriteLine("Current_BufferingStarted() - Waiting: " + BackgroundMediaPlayer.Current.BufferingProgress);
+                    await Task.Delay(5000);
+                }
+                BackgroundMediaPlayer.Current.Play();
+            }
+            catch (Exception)
+            {
+
+            }            
+        }
+
+        void Task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            BackgroundMediaPlayer.Shutdown();
+            _deferral.Complete();
+        }
+
+        void taskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            _isBackgroundTaskRunning = false;         
+
+            // Sending message to null foreground mediaplayer
+            var message = new ValueSet { { "Exited", "" } };
+            BackgroundMediaPlayer.SendMessageToForeground(message);
+
+            BackgroundMediaPlayer.Shutdown();
+            _deferral.Complete();
+        }
+
+        void Current_CurrentStateChanged(MediaPlayer sender, object args)
+        {
+            switch (sender.CurrentState)
+            {   
+                case MediaPlayerState.Closed:
+                    break;
+                case MediaPlayerState.Opening:
+                    break;
+                case MediaPlayerState.Paused:
+                    _systemMediaTransportControl.PlaybackStatus = MediaPlaybackStatus.Paused;
+                    break;
+                case MediaPlayerState.Buffering:
+                    break;
+                case MediaPlayerState.Playing:
+                    _systemMediaTransportControl.PlaybackStatus = MediaPlaybackStatus.Playing;
+                    break;
+                case MediaPlayerState.Stopped:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void BackgroundMediaPlayer_MessageReceivedFromForeground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            ValueSet valueSet = e.Data;
+            foreach (string key in valueSet.Keys)
+            {
+                switch (key)
+                {
+                    case "Play":
+                        PlayMedia(valueSet[key].ToString());
+                        break;
+                    case "Pause":
+                        PauseMedia();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void PauseMedia()
+        {
+            var mediaPlayer = BackgroundMediaPlayer.Current;
+            mediaPlayer.Pause();
+        }
+
+        private void PlayMedia(string toPlay)
+        {
+            try
+            {
+                var mediaPlayer = BackgroundMediaPlayer.Current;
+                mediaPlayer.SetUriSource(new Uri(toPlay));
+                mediaPlayer.AutoPlay = true;
+
+                _nowPlaying = toPlay;
+
+                _systemMediaTransportControl.DisplayUpdater.Type = MediaPlaybackType.Music;
+                _systemMediaTransportControl.DisplayUpdater.MusicProperties.Title = "Aristocrats FM";
+                _systemMediaTransportControl.DisplayUpdater.Update();
+            }
+            catch (Exception)
+            {
+                // TODO Make message for foreground about error
+                //BackgroundMediaPlayer.Shutdown();
+                //_deferral.Complete();
+            }            
+        }
+
+        private void MediaTransportControlButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs args)
+        {
+            switch (args.Button)
+            {
+                case SystemMediaTransportControlsButton.Pause:
+                    PauseMedia();
+                    break;
+                case SystemMediaTransportControlsButton.Play:
+                    if (!_isBackgroundTaskRunning)
+                    {
+                        _BackgroundStarted.WaitOne(2000);
+                    }
+                    PlayMedia(_nowPlaying);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
